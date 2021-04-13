bfae1ce62ed03f46d983f2e9d6c7af51d84cd823
Animuslupus <16310725+Animuslupus@users.noreply.github.com>
2021-04-13 17:23:24 +0200

    Format and Fix App.js

--- fat/src/App.js
+++ fat/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import {Container} from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import { Trans } from 'react-i18next';
 import i18n from './i18n';
 
@@ -11,7 +11,7 @@ import 'semantic-ui-css/semantic.min.css'
 import AlternativeList from "./Components/AlternativeList";
 import SearchBar from "./Components/SearchBar";
 import AlternativeDetails from "./Components/AlternativeDetails";
-import {pushEvent, fetchProducts} from './helper';
+import { pushEvent, fetchProducts } from './helper';
 
 class App extends React.Component {
 
@@ -19,23 +19,24 @@ class App extends React.Component {
         super(props);
         this.state = {
             detailsSelected: false,
-            products:[],
-            loading:false,
+            products: [],
+            loading: false,
         };
-
-
+        window.addEventListener("beforeunload", (e) => {
+            pushEvent('UserLeave')
+        })
     }
 
 
 
     loadProducts() {
-        fetchProducts().then((result)=>{
-            var prodList=[];
-            for (var x in result){
+        fetchProducts().then((result) => {
+            var prodList = [];
+            for (var x in result) {
 
-                var y ={
+                var y = {
                     ...result[x],
-                    title:result[x].nameGerman
+                    title: result[x].nameGerman
                 }
                 prodList.push(y)
             }
@@ -50,14 +51,14 @@ class App extends React.Component {
 
     //change lng of internatinalization package
     //'en' or 'de' are the implemented languages
-    changeLanguage(lng){
+    changeLanguage(lng) {
         i18n.changeLanguage(lng);
     }
 
     getOverviewOrDetails() {
         if (this.state.detailsSelected) {
             return (
-                <AlternativeDetails/>
+                <AlternativeDetails />
 
             )
         } else
@@ -66,41 +67,36 @@ class App extends React.Component {
                     <SearchBar
                         products={this.state.products}
                     />
-                    <AlternativeList/>
-                </Container>
+                    <AlternativeList />
+                </Container >
             )
     };
 
-    componentDidMount(){
-        pushEvent('UserJoin', '');
+    componentDidMount() {
+        pushEvent('UserJoin');
 
         this.loadProducts();
     }
 
-
-    componentWillUnmount(){
-        pushEvent('UserLeave', '')
-    }
-
     render() {
         return (
-                <Container textAlign="center" style={{marginTop: '1em', paddingLeft: '10em', paddingRight: '10em'}}
-                           fluid>
+            <Container textAlign="center" style={{ marginTop: '1em', paddingLeft: '10em', paddingRight: '10em' }}
+                fluid>
 
-                    <header>
-                        <p>
-                            <Trans >
-                                Welcome
+                <header>
+                    <p>
+                        <Trans >
+                            Welcome
                             </Trans>
-                        </p>
-                    </header>
+                    </p>
+                </header>
 
-                    {this.getOverviewOrDetails()}
-
-
+                {this.getOverviewOrDetails()}
 
 
-                </Container>
+
+
+            </Container>
         )
     }
 }
