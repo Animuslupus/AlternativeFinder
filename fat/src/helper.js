5501810c7d50a4631cba07bb8f918ed0888b505f
Animuslupus <16310725+Animuslupus@users.noreply.github.com>
2021-04-13 16:37:14 +0200

    Add ip address & Dev check to events

--- fat/src/helper.js
+++ fat/src/helper.js
@@ -1,19 +1,25 @@
-import {appConfig, eventDB, firebase} from './config'
+import { appConfig, eventDB, firebase } from './config'
+import publicIp from "public-ip"
 
-async function fetchProducts (){
+async function fetchProducts() {
     const ip = appConfig.isDev ? appConfig.devApiIp : appConfig.productionApiIp;
     var result = await fetch(ip + '/products')
     result = await result.json()
     return result
 }
 
-function pushEvent(eventType, userId, params={}){
-    eventDB.collection("FATEvents").add({
-        type:eventType,
-        userId:userId,
-        createdAt: firebase.firestore.Timestamp.now(),
-        params: params
+async function pushEvent(eventType, userId, params = {}) {
+    const ip = await publicIp.v4({
+        fallbackUrls: ["https://ifconfig.co/ip"]
     })
+    if (!appConfig.isDev)
+        eventDB.collection("FATEvents").add({
+            type: eventType,
+            userId: userId,
+            ipAddress: ip,
+            createdAt: firebase.firestore.Timestamp.now(),
+            params: params
+        })
 }
 
-export {fetchProducts, pushEvent};
+export { fetchProducts, pushEvent };
