30ae30b7460a1db21d9b3d452266630d2975ccaa
Frederick Gamer <frederick.gamer@resolutio.de>
2021-04-13 10:34:01 +0200

    Add semantic ui Add view elements

---
+++ fat/src/AlternativeList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class AlternativeList extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Here is a list of alternatives
+            </div>
+        )
+    }
+}
+
+export default AlternativeList
---
+++ fat/src/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Here is the product search
+            </div>
+        )
+    }
+}
+
+export default SearchBar
