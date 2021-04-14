0c1c36d1795fbe08294aeb0fa4ac340094c5874f
Animuslupus <16310725+Animuslupus@users.noreply.github.com>
2021-04-14 13:33:00 +0200

    Update alternativeList

--- fat/src/Components/AlternativeList.js
+++ fat/src/Components/AlternativeList.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import AlternativeCard from './AlternativeCard';
 
 
 class AlternativeList extends React.Component {
 
+    // this.props.products
+
+    render_alternative(product) {
+        console.log(this.props.products)
+        return product['alternatives'].map(alt => <Grid.Column key={alt['id']}><AlternativeCard product={product} alternative={alt} /></Grid.Column>)
+    }
+
     render() {
-        return (
-            <Grid stackable>
-                
-            </Grid>
-        )
+        if (this.props.products && this.props.products.length > 0 && this.props.products[0])
+            return (
+                <Grid doubling stackable columns={4}>
+                    {this.props.products.map(p => this.render_alternative(p))}
+                </Grid>
+            )
+        else
+            return <></>
     }
 }
 
