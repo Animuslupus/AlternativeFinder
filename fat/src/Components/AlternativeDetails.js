2751a4af15c618856d32110ae88b355b6d614339
Animuslupus <16310725+Animuslupus@users.noreply.github.com>
2021-04-14 15:23:53 +0200

    Add percentage to AlternativeDetails

--- fat/src/Components/AlternativeDetails.js
+++ fat/src/Components/AlternativeDetails.js
@@ -29,7 +29,8 @@ class AlternativeDetails extends React.Component {
     render() {
         if (!this.props.alternative || !this.props.product)
             return (<></>)
-        else
+        else {
+            const betterInPercentage = Math.round(((this.props.product['emissions'] - this.props.alternative['emissions']) / this.props.product['emissions']) * 100)
             return (
                 <Modal
                     centered={false}
@@ -40,38 +41,47 @@ class AlternativeDetails extends React.Component {
                             <Grid columns={2} verticalAlign='middle'>
                                 <Grid.Column>
                                     <Image style={{ margin: 'auto' }}
+                                        size='small'
                                         src={this.state.product.imageLink}
                                     />
                                 </Grid.Column>
                                 <Grid.Column>
                                     <Image style={{ margin: 'auto' }}
+                                        size='small'
+                                        src={this.state.product.imageLink}
                                         src={this.state.alternative.imageLink}
                                     />
                                 </Grid.Column>
                             </Grid>
                             <Divider vertical><p>REPLACED BY</p></Divider>
                         </Segment>
-                        <Container style={{paddingLeft:'20%', paddingRight:'20%', paddingBottom: '2em', paddingTop: '1em' }}>
+                        <Container style={{ paddingLeft: '20%', paddingRight: '20%', paddingBottom: '2em', paddingTop: '1em' }}>
                             <Header as='h2'>{this.state.product.name} replaced by {this.state.alternative.name}</Header>
                             <p>
                                 <MarkdownRenderer markdown={this.state.alternative.description} />
                             </p>
                         </Container>
+                        <Divider />
                         <Container textAlign="center">
-                            <Header as='h3'>Absolute Impact</Header>
+                            <Header as='h3'>Impact</Header>
+                            <Card style={{ margin: 'auto', padding: '2em' }}>
+                                <b>
+                                    {betterInPercentage}% more sustainable
+                                </b>
+                            </Card>
                             <Segment>
                                 <Grid columns={2}>
                                     <Grid.Column>
                                         <Card style={{ margin: 'auto', padding: '2em', backgroundColor: '#FF5151' }}>
                                             <b>
-                                                {this.state.product.emissions}
+                                                {this.state.product.emissions} {this.state.product.unit}
                                             </b>
                                         </Card>
                                     </Grid.Column>
                                     <Grid.Column>
                                         <Card style={{ margin: 'auto', padding: '2em', backgroundColor: '#F2EC64' }}>
                                             <b>
-                                                {this.state.alternative.emissions}
+                                                {this.state.alternative.emissions} {this.state.alternative.unit}
                                             </b>
                                         </Card>
                                     </Grid.Column>
@@ -82,6 +92,7 @@ class AlternativeDetails extends React.Component {
                     </Container>
                 </Modal>
             )
+        }
     }
 }
 
