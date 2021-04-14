a60c1cb009dcc4bd1b73fbb88526ca14ef622ba1
Animuslupus <16310725+Animuslupus@users.noreply.github.com>
2021-04-14 14:40:48 +0200

    Clean up modal for alternative-details

--- fat/src/Components/AlternativeDetails.js
+++ fat/src/Components/AlternativeDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Modal, Container, Image, Divider, Grid, Header, Segment, Card} from 'semantic-ui-react'
+import { Modal, Container, Image, Divider, Grid, Header, Segment, Card } from 'semantic-ui-react'
 
 class AlternativeDetails extends React.Component {
 
@@ -23,71 +23,64 @@ class AlternativeDetails extends React.Component {
             alternative: nextProps.alternative,
             product: nextProps.product
         });
-        console.log(nextProps.product)
     }
 
     render() {
-        return (
-            <Modal
-                open={this.state.isOpen}
-                onClose={() => {
-                    this.setState({
-                        isOpen: false
-                    });
-                }
-                }>
-                <Container>
-                    <Container style={{backgroundColor: '#1a531b', margin: '2em', padding: '2em'}}>
-                        <Segment style={{backgroundColor: '#1a531b'}}>
-                            <Grid columns={2}>
-                                <Grid.Column>
-                                    <Image style={{margin: 'auto'}}
-                                           src={this.state.product.imageLink}
-                                    />
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Image style={{margin: 'auto'}}
-                                           src={this.state.alternative.imageLink}
-                                    />
-                                </Grid.Column>
-                            </Grid>
-                            <Divider vertical>REPLACED BY</Divider>
-                        </Segment>
-                    </Container>
-                    <Container
-                        style={{paddingLeft: '20%', paddingRight: '20%', paddingBottom: '2em', paddingTop: '1em'}}>
-                        <Header as='h2'>{this.state.product.name} replaced by {this.state.alternative.name}</Header>
-                        <p>
-                            {this.state.alternative.description}
-                        </p>
-                    </Container>
-                    <Divider>
-                    </Divider>
-                    <Container textAlign="center">
-                        <Header as='h3'>Absolute Impact</Header>
+        if (!this.props.alternative || !this.props.product)
+            return (<></>)
+        else
+            return (
+                <Modal
+                    centered={false}
+                    open={this.state.isOpen}
+                    onClose={this.props.onClose}>
+                    <Container>
                         <Segment>
-                            <Grid columns={2}>
+                            <Grid columns={2} verticalAlign='middle'>
                                 <Grid.Column>
-                                    <Card style={{margin: 'auto', padding: '2em', backgroundColor:'#FF5151'}}>
-                                        <b>
-                                            {this.state.product.emissions}
-                                        </b>
-                                    </Card>
+                                    <Image style={{ margin: 'auto' }}
+                                        src={this.state.product.imageLink}
+                                    />
                                 </Grid.Column>
                                 <Grid.Column>
-                                    <Card style={{margin: 'auto', padding: '2em', backgroundColor:'#F2EC64'}}>
-                                        <b>
-                                            {this.state.alternative.emissions}
-                                        </b>
-                                    </Card>
+                                    <Image style={{ margin: 'auto' }}
+                                        src={this.state.alternative.imageLink}
+                                    />
                                 </Grid.Column>
                             </Grid>
-                            <Divider vertical>VS</Divider>
+                            <Divider vertical><p>REPLACED BY</p></Divider>
                         </Segment>
+                        <Container style={{paddingLeft:'20%', paddingRight:'20%', paddingBottom: '2em', paddingTop: '1em' }}>
+                            <Header as='h2'>{this.state.product.name} replaced by {this.state.alternative.name}</Header>
+                            <p>
+                                {this.state.alternative.description}
+                            </p>
+                        </Container>
+                        <Container textAlign="center">
+                            <Header as='h3'>Absolute Impact</Header>
+                            <Segment>
+                                <Grid columns={2}>
+                                    <Grid.Column>
+                                        <Card style={{ margin: 'auto', padding: '2em', backgroundColor: '#FF5151' }}>
+                                            <b>
+                                                {this.state.product.emissions}
+                                            </b>
+                                        </Card>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Card style={{ margin: 'auto', padding: '2em', backgroundColor: '#F2EC64' }}>
+                                            <b>
+                                                {this.state.alternative.emissions}
+                                            </b>
+                                        </Card>
+                                    </Grid.Column>
+                                </Grid>
+                                <Divider vertical>VS</Divider>
+                            </Segment>
+                        </Container>
                     </Container>
-                </Container>
-            </Modal>
-        )
+                </Modal>
+            )
     }
 }
 
